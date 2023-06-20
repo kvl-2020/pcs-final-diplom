492ffd40d35b8a9b30a82497435b52b31de79e81
Kozyaruk Vadim <kozyaruk@mail.ru>
2023-06-20 22:58:55 +0300

    Основное задание выполнено. Поисковый движок, сервер, клиент для проверки.

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,23 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    public String toString() {
+        return pdfName + " - " + page + " - " + count + ";";
+    }
+
+    public String toStringAsJSON() {
+        return "\t{\n" + "\t\t\"pdfName\" : " + pdfName + ",\n" + "\t\t\"page\" : " + page + ",\n" + "\t\t\"count\" : " + count + "\n" + "\t}";
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        return o.count - this.count;
+    }
+
 }
